451db9672ffba6c4cce55fcdd1595b0989f89f12
Darsh Shah <darsh0908@gmail.com>
2022-03-09 19:31:39 +0530

    Kanban and files

---
+++ components/documentation.js
@@ -0,0 +1,11 @@
+import React from 'react';
+
+const Documentation = () => {
+    return (
+        <div>
+            <iframe className='w-[80vw]' src="https://docs.google.com/document/d/e/2PACX-1vTpfNO5BdJGmHtuSL3RaiQlw_n-inGEo336eC3qT78zAt_NmBlLv-2UWKyNln2Kevl-uErd71xiP7WN/pub?embedded=true"></iframe>
+        </div>
+    );
+}
+
+export default Documentation;
