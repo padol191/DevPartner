8c37d3167e5506c1fbc117e4addd5ef7ff91e4eb
padol191 <Chaitanyapadol@icloud.com>
2022-03-10 08:28:16 +0530

    header

--- components/home.js
+++ components/home.js
@@ -113,9 +113,14 @@ export default function Navbar() {
             </div>
           </Link>
           <div className="hidden sm:flex flex-row space-x-4">
-            <Link href="/">
+            <Link href="/register">
+              <button className="rounded-md flex space-x-2 w-24 h-10 font-normal text-sm leading-3 text-indigo-700 bg-white border border-indigo-700 focus:outline-none focus:bg-gray-200 hover:bg-gray-200 duration-150 justify-center items-center">
+                Sign Up
+              </button>
+            </Link>
+            <Link href="/login">
               <button className="rounded-md flex space-x-2 w-24 h-10 font-normal text-sm leading-3 text-white bg-indigo-700 focus:outline-none focus:ring-2 focus:ring-offset-2 focus:ring-indigo-700 focus:bg-indigo-600 hover:bg-indigo-600 duration-150 justify-center items-center">
-                Logout
+                Sign In
               </button>
             </Link>
           </div>
--- components/layout.js
+++ components/layout.js
@@ -1,4 +1,4 @@
-import Navbar from "./navbar";
+import Navbar from "./home";
 import Footer from "./footer";
 
 export default function Layout({ children }) {
